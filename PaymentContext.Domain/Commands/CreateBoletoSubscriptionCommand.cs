95bcf50abfaeb286ccbc8a5b365690b62721ba6a
tsci <tsci@gft.com>
2021-06-08 18:53:11 -0300

    fail fast validation

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,9 +1,12 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -26,5 +29,15 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+        public void Validate()
+        {
+            // contract -> não precisa testar, ele já testa para vc
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome deve conter até 40 caracteres")
+            );
+        }
     }
 }
--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -16,14 +16,6 @@ namespace PaymentContext.Domain.ValueObjects
 
             // if(string.IsNullOrEmpty(LastName))
             //     AddNotification("Name.FirstName", "Sobrenome inválido!");
-
-            // contract -> não precisa testar, ele já testa para vc
-            AddNotifications(new Contract()
-                .Requires()
-                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
-                .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
-                .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome deve conter até 40 caracteres")
-            );
         }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
